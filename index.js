f45fd1a7d6629a972bc66c645640caf6af55b4f4
Jake Champion <jakechampion.jake2@googlemail.com>
2018-03-21 17:30:18 +0000

    Migrate to new webpack 4 plugin system

--- index.js
+++ index.js
@@ -1,42 +1,78 @@
 'use strict';
 
 let path = require('path');
+const pluginName = "bower-resolve-webpack-plugin";
 
 module.exports = class BowerResolvePlugin {
     constructor(options) {
         this.options = options;
-    }
+	}
 
     apply(resolver) {
-        resolver.plugin('existing-directory', function (request, callback) {
-            if (request.path !== request.descriptionFileRoot) {
-                return callback();
-            }
-
-            let mainModule = null;
-            
-            if (request.descriptionFilePath.endsWith('bower.json')) {
-                let source = request.descriptionFileData;
-                let modules = typeof source['main'] == 'string'
-                    ? [source['main']]
-                    : source['main'];
-
-                if (! modules) {
+        // Webpack 4
+        if (Object.prototype.hasOwnProperty.call(resolver, 'hooks')) {
+            resolver.hooks.existingDirectory.tapAsync(pluginName, (request, resolveContext, callback) => {
+                if (request.path !== request.descriptionFileRoot) {
                     return callback();
                 }
 
-                mainModule = modules.find(module => module.endsWith('.js') );
-            }
+                let mainModule = null;
 
-            if (! mainModule) {
-                return callback();
-            }
+                if (request.descriptionFilePath.endsWith('bower.json')) {
+                    let source = request.descriptionFileData;
+                    let modules = typeof source['main'] == 'string'
+                        ? [source['main']]
+                        : source['main'];
 
-            let object = Object.assign({}, request, {
-                path: path.resolve(request.path, mainModule)
+                    if (!modules) {
+                        return callback();
+                    }
+
+                    mainModule = modules.find(module => module.endsWith('.js'));
+                }
+
+                if (!mainModule) {
+                    return callback();
+                }
+
+                let object = Object.assign({}, request, {
+                    path: path.resolve(request.path, mainModule)
+                });
+
+                return resolver.doResolve(resolver.hooks.undescribedRawFile, object, 'using path: ' + mainModule, resolveContext, callback);
             });
+        // Webpack 2 or 3
+        } else {
+            resolver.plugin('existing-directory', function (request, callback) {
+                if (request.path !== request.descriptionFileRoot) {
+                    return callback();
+                }
 
-            return resolver.doResolve('undescribed-raw-file', object, 'using path: ' + mainModule, callback);
-        });
+                let mainModule = null;
+
+                if (request.descriptionFilePath.endsWith('bower.json')) {
+                    let source = request.descriptionFileData;
+                    let modules = typeof source['main'] == 'string'
+                        ? [source['main']]
+                        : source['main'];
+
+                    if (! modules) {
+                        return callback();
+                    }
+
+                    mainModule = modules.find(module => module.endsWith('.js') );
+                }
+
+                if (! mainModule) {
+                    return callback();
+                }
+
+                let object = Object.assign({}, request, {
+                    path: path.resolve(request.path, mainModule)
+                });
+
+                return resolver.doResolve('undescribed-raw-file', object, 'using path: ' + mainModule, callback);
+            });
+        }
     }
 }
